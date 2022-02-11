9da290736fead8590bb6ad4ba93ed6bca2e0f027
ojitos369 <ojitos369@gmail.com>
2022-02-11 16:18:22 -0600

    start models

--- ojitos369/ojitos369/settings.py
+++ ojitos369/ojitos369/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'psico_api.apps.PsicoApiConfig',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

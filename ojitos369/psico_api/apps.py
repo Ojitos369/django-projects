6f5c3cf95602829eb82e21656a51513b9ec5dcac
ojitos369 <ojitos369@gmail.com>
2022-02-11 12:48:51 -0600

    init psico

---
+++ ojitos369/psico_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PsicoApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'psico_api'
---
+++ ojitos369/psico_front/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PsicoFrontConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'psico_front'

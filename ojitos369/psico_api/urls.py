99dd651f9e0905a33664cb2567bf95de4053c093
ojitos369 <ojitos369@gmail.com>
2022-02-11 20:50:46 -0600

    add get test

--- ojitos369/psico_api/urls.py
+++ ojitos369/psico_api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = 'psico_api'
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('get_test/<int:test_id>/', views.get_test, name='get_test'),
 ]
--- ojitos369/psico_api/views.py
+++ ojitos369/psico_api/views.py
@@ -1,3 +1,29 @@
+# Apis for psico_api
+# Django
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.forms.models import model_to_dict
+# Django Rest Framework
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+# User
+from .models import *
+# Python
+import json
 
-# Create your views here.
+@api_view(['GET'])
+def index(request):
+    return Response({"message": "Hello, world!"}, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def get_test(request, test_id):
+    test = Test.objects.get(pk=test_id)
+    test = model_to_dict(test)
+    test['url'] = f'/psico_front/test/{test_id}.html'
+    return Response(test, status=status.HTTP_200_OK)
